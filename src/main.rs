18e8dbd0d28d4f8402a2bb65e393fa3caff92c47
qthree <qthree3@gmail.com>
2021-11-06 01:02:37 +0700

    Non-working version for f1

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,18 @@ use panic_itm;
 use cortex_m::iprintln;
 use cortex_m_rt::entry;
 //use stm32f3_discovery::{leds::Leds, stm32f3xx_hal, switch_hal::{ToggleableOutputSwitch, OutputSwitch}};
-use stm32f3xx_hal::{hal::digital::v2::{IoPin, PinState}, prelude::*};
-pub use stm32f3xx_hal::{
+use stm32f1xx_hal as hal;
+use hal::{
+    prelude::*,
     delay::Delay,
     gpio::{Output, PushPull},
-    hal::blocking::delay::DelayMs,
     pac, usb,
 };
+use embedded_hal::{blocking::delay::DelayMs, digital::v2::{IoPin, PinState}};
 
 use heapless::String;
 
+/*
 macro_rules! input_pins {
     ($($gpio:expr => [$($pin:ident),*]),*) => {
         [$(
@@ -37,32 +39,67 @@ macro_rules! output_pins {
         )*]
     }
 }
+*/
+
+macro_rules! input_pins {
+    ($($gpio:expr; $cr:ident => [$($pin:ident),*]),*) => {
+        [$(
+            $(
+                $gpio.$pin.into_pull_up_input(&mut $gpio.$cr).erase(),
+            )*
+        )*]
+    }
+}
+macro_rules! output_pins {
+    ($($gpio:expr; $cr:ident => [$($pin:ident),*]),*) => {
+        [$(
+            $(
+                $gpio.$pin.into_push_pull_output(&mut $gpio.$cr).erase(),
+            )*
+        )*]
+    }
+}
 
 #[entry]
 fn main() -> ! {
     let device_periphs = pac::Peripherals::take().unwrap();
-    let mut reset_and_clock_control = device_periphs.RCC.constrain();
+    let mut rcc = device_periphs.RCC.constrain();
 
     let core_periphs = cortex_m::Peripherals::take().unwrap();
     let mut flash = device_periphs.FLASH.constrain();
     //let clocks = reset_and_clock_control.cfgr.freeze(&mut flash.acr);
-    let clocks = reset_and_clock_control.cfgr
+    let clocks = rcc.cfgr
+        /*
         .use_hse(8.MHz())
         .sysclk(48.MHz())
         .pclk1(24.MHz())
         .pclk2(24.MHz())
+        */
+        .use_hse(8.mhz())
+        .sysclk(48.mhz())
+        .pclk1(24.mhz())
         .freeze(&mut flash.acr);
+
+    assert!(clocks.usbclk_valid());
+
     let mut delay = Delay::new(core_periphs.SYST, clocks);
 
     let mut itm = core_periphs.ITM;
 
-    // initialize user leds
+    
+    /*
     let mut gpioa = device_periphs.GPIOA.split(&mut reset_and_clock_control.ahb);
     let mut gpiob = device_periphs.GPIOB.split(&mut reset_and_clock_control.ahb);
     let mut gpiod = device_periphs.GPIOD.split(&mut reset_and_clock_control.ahb);
     let mut gpioe = device_periphs.GPIOE.split(&mut reset_and_clock_control.ahb);
+    */
 
-    let mut led = gpioe.pe8.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut gpioa = device_periphs.GPIOA.split();
+    let mut gpiob = device_periphs.GPIOB.split();
+    let mut gpioc = device_periphs.GPIOC.split();
+
+    //let mut led = gpioe.pe8.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
     /*    
     let mut leds = Leds::new(
@@ -78,7 +115,8 @@ fn main() -> ! {
         &mut gpioe.otyper,
     ).into_array();
     */
-    
+   
+    /*
     let cols = input_pins!(
         gpiob => [pb10, pb12, pb14],
         gpiod => [pd8, pd10, pd12]
@@ -87,6 +125,18 @@ fn main() -> ! {
     let rows = output_pins!(
         gpiod => [pd13, pd11, pd9],
         gpiob => [pb15, pb13, pb11]
+    );
+    */
+
+    
+    let cols = input_pins!(
+        gpioa; crl => [pa0, pa1, pa2, pa3, pa4, pa5]
+    );
+    
+    let rows = output_pins!(
+        gpioa; crl => [pa6, pa7],
+        gpiob; crl => [pb0, pb1],
+        gpiob; crh => [pb10, pb11]
     );
 
     use keyberon::{
@@ -100,6 +150,7 @@ fn main() -> ! {
     let mut debouncer = Debouncer::new(PressedKeys::<6, 6>::default(), PressedKeys::default(), 5);
     let mut layout = Layout::new(crate::layout::LAYERS);
 
+    /*
     // F3 Discovery board has a pull-up resistor on the D+ line.
     // Pull the D+ pin down to send a RESET condition to the USB bus.
     // This forced reset is needed only for development, without it host
@@ -115,6 +166,22 @@ fn main() -> ! {
             .pa11
             .into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
     let pin_dp = usb_dp.into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+    */
+
+    // BluePill board has a pull-up resistor on the D+ line.
+    // Pull the D+ pin down to send a RESET condition to the USB bus.
+    // This forced reset is needed only for development, without it host
+    // will not reset your device when you upload new firmware.
+    let pin_dp = {
+        /*
+        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+        usb_dp.set_low();
+        cortex_m::asm::delay(clocks.sysclk().0 / 100);
+        usb_dp.into_floating_input(&mut gpioa.crh)
+        */
+        gpioa.pa12 //.into_floating_input(&mut gpioa.crh)
+    };
+    let pin_dm = gpioa.pa11;
 
     let usb = usb::Peripheral {
         usb: device_periphs.USB,
@@ -140,18 +207,18 @@ fn main() -> ! {
     let mut blink = 0;
     loop {
         if blink == 0{
-            led.toggle().unwrap();
+            let _ = led.toggle();
             blink = 1000;
         } else {
             blink -= 1;
         }
 
         //let pressed_keys = matrix.get().unwrap();
-        for event in debouncer
+        /*for event in debouncer
             .events(matrix.get().unwrap())
         {
             layout.event(event);
-        }
+        }*/
 
         if usb_dev.poll(&mut [&mut usb_class]) {
             use usb_device::class::UsbClass as _;
