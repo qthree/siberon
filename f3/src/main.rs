f7858ed229e25c54e0831ed25691acc54e8cc48d
qthree <qthree3@gmail.com>
2021-11-07 22:53:25 +0700

    Make it compile for both f1 and f3

--- f3/src/main.rs
+++ f3/src/main.rs
@@ -7,7 +7,7 @@ use panic_itm;
 use cortex_m::iprintln;
 use cortex_m_rt::entry;
 //use stm32f3_discovery::{leds::Leds, stm32f3xx_hal, switch_hal::{ToggleableOutputSwitch, OutputSwitch}};
-use stm32f1xx_hal as hal;
+use stm32f3xx_hal as hal;
 use hal::{
     prelude::*,
     delay::Delay,
@@ -20,7 +20,7 @@ use heapless::String;
 
 use siberon::{layout, keyberon};
 
-/*
+
 macro_rules! input_pins {
     ($($gpio:expr => [$($pin:ident),*]),*) => {
         [$(
@@ -39,26 +39,6 @@ macro_rules! output_pins {
         )*]
     }
 }
-*/
-
-macro_rules! input_pins {
-    ($($gpio:expr; $cr:ident => [$($pin:ident),*]),*) => {
-        [$(
-            $(
-                $gpio.$pin.into_pull_up_input(&mut $gpio.$cr).erase(),
-            )*
-        )*]
-    }
-}
-macro_rules! output_pins {
-    ($($gpio:expr; $cr:ident => [$($pin:ident),*]),*) => {
-        [$(
-            $(
-                $gpio.$pin.into_push_pull_output(&mut $gpio.$cr).erase(),
-            )*
-        )*]
-    }
-}
 
 #[entry]
 fn main() -> ! {
@@ -67,58 +47,26 @@ fn main() -> ! {
 
     let core_periphs = cortex_m::Peripherals::take().unwrap();
     let mut flash = device_periphs.FLASH.constrain();
-    //let clocks = reset_and_clock_control.cfgr.freeze(&mut flash.acr);
+
     let clocks = rcc.cfgr
-        /*
         .use_hse(8.MHz())
         .sysclk(48.MHz())
         .pclk1(24.MHz())
         .pclk2(24.MHz())
-        */
-        .use_hse(8.mhz())
-        .sysclk(48.mhz())
-        .pclk1(24.mhz())
         .freeze(&mut flash.acr);
 
     assert!(clocks.usbclk_valid());
 
     let mut delay = Delay::new(core_periphs.SYST, clocks);
 
-    //let mut itm = core_periphs.ITM;
-    //iprintln!(&mut itm.stim[0], "\n\n\n\n");
+    let mut gpioa = device_periphs.GPIOA.split(&mut rcc.ahb);
+    let mut gpiob = device_periphs.GPIOB.split(&mut rcc.ahb);
+    let mut gpiod = device_periphs.GPIOD.split(&mut rcc.ahb);
+    let mut gpioe = device_periphs.GPIOE.split(&mut rcc.ahb);
 
-    
-    /*
-    let mut gpioa = device_periphs.GPIOA.split(&mut reset_and_clock_control.ahb);
-    let mut gpiob = device_periphs.GPIOB.split(&mut reset_and_clock_control.ahb);
-    let mut gpiod = device_periphs.GPIOD.split(&mut reset_and_clock_control.ahb);
-    let mut gpioe = device_periphs.GPIOE.split(&mut reset_and_clock_control.ahb);
-    */
-
-    let mut gpioa = device_periphs.GPIOA.split();
-    let mut gpiob = device_periphs.GPIOB.split();
-    let pb13 = gpiob.pb3;
-    let mut gpioc = device_periphs.GPIOC.split();
-
-    //let mut led = gpioe.pe8.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    /*    
-    let mut leds = Leds::new(
-        gpioe.pe8,
-        gpioe.pe9,
-        gpioe.pe10,
-        gpioe.pe11,
-        gpioe.pe12,
-        gpioe.pe13,
-        gpioe.pe14,
-        gpioe.pe15,
-        &mut gpioe.moder,
-        &mut gpioe.otyper,
-    ).into_array();
-    */
+    let mut led = gpioe.pe8.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
    
-    /*
+    
     let cols = input_pins!(
         gpiob => [pb10, pb12, pb14],
         gpiod => [pd8, pd10, pd12]
@@ -127,18 +75,6 @@ fn main() -> ! {
     let rows = output_pins!(
         gpiod => [pd13, pd11, pd9],
         gpiob => [pb15, pb13, pb11]
-    );
-    */
-
-    
-    let cols = input_pins!(
-        gpioa; crl => [pa0, pa1, pa2, pa3, pa4, pa5]
-    );
-    
-    let rows = output_pins!(
-        gpioa; crl => [pa6, pa7],
-        gpiob; crl => [pb0, pb1],
-        gpiob; crh => [pb10, pb11]
     );
 
     use keyberon::{
@@ -152,7 +88,6 @@ fn main() -> ! {
     let mut debouncer = Debouncer::new(PressedKeys::<6, 6>::default(), PressedKeys::default(), 5);
     let mut layout = Layout::new(crate::layout::LAYERS);
 
-    /*
     // F3 Discovery board has a pull-up resistor on the D+ line.
     // Pull the D+ pin down to send a RESET condition to the USB bus.
     // This forced reset is needed only for development, without it host
@@ -168,20 +103,6 @@ fn main() -> ! {
             .pa11
             .into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
     let pin_dp = usb_dp.into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
-    */
-
-    // BluePill board has a pull-up resistor on the D+ line.
-    // Pull the D+ pin down to send a RESET condition to the USB bus.
-    // This forced reset is needed only for development, without it host
-    // will not reset your device when you upload new firmware.
-    let pin_dp = {
-        
-        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-        usb_dp.set_low();
-        cortex_m::asm::delay(clocks.sysclk().0 / 100);
-        usb_dp.into_floating_input(&mut gpioa.crh)
-    };
-    let pin_dm = gpioa.pa11;
 
     let usb = usb::Peripheral {
         usb: device_periphs.USB,
@@ -192,17 +113,6 @@ fn main() -> ! {
     let usb_bus = usb::UsbBusType::new(usb);
     let mut usb_class = keyberon::new_class(&usb_bus, ());
     let mut usb_dev = keyberon::new_device(&usb_bus);
-    
-
-    /*let input = gpioc.pc6.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
-    let mut output = gpioc.pc8.into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
-    output.set_low().unwrap();*/
-    
-    //let input = gpioc.pc8.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
-    //let mut output = gpioc.pc6.into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
-    //output.set_low().unwrap();
-
-    //let mut buffer: String<255> = String::new();
 
     let mut blink = 0;
     loop {
@@ -246,29 +156,5 @@ fn main() -> ! {
                 }
             }
         }
-        
-        /*buffer.clear();
-        for (row_i, row) in pressed_keys.0.iter().enumerate() {
-            for (col_i, button) in row.iter().enumerate() {
-                buffer.push_str(" [").unwrap();
-                if *button {
-                    buffer.push((b'A' + col_i as u8) as char).unwrap();
-                    buffer.push((b'1' + row_i as u8) as char).unwrap();
-                } else {
-                    buffer.push_str("  ").unwrap();
-                }
-                buffer.push_str("] ").unwrap();
-            }    
-            buffer.push('\n').unwrap();
-        }
-        iprintln!(&mut itm.stim[0], "\n\n\n\n{}", buffer);
-        */
-
-        /*if input.is_low().unwrap() {
-            leds[1].on().unwrap();
-        } else {
-            leds[1].off().unwrap();
-        }*/
-        //delay.delay_ms(200u16);
     }
 }
